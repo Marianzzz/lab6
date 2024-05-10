2d9b3acb43f7ac1af80c4ea784ba0bd50c33254e
Marian Tsyvinskyi <mariantop228@gmail.com>
2024-05-10 19:26:25 +0300

    create second pattern strategy

---
+++ patterns/strategy.py
@@ -0,0 +1,11 @@
+class MessageProcessingStrategy:
+    def process_message(self, message):
+        pass
+
+class BasicMessageProcessingStrategy(MessageProcessingStrategy):
+    def process_message(self, message):
+        pass
+
+class AdvancedMessageProcessingStrategy(MessageProcessingStrategy):
+    def process_message(self, message):
+        pass
